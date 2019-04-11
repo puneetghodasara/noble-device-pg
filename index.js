bed213f4d2a8d17ffaab5e7f0db0e72adce4d307
Punit Ghodasara <punit.ghodasara@sap.com>
2019-04-11 11:03:23 +0100

    Exported both classes

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const nobleDeviceFactory = require('./lib/deviceFactory');
+const device = require('./lib/device');
+
 module.exports.nobleDeviceFactory = nobleDeviceFactory;
+module.exports.nobleDevice = device;
